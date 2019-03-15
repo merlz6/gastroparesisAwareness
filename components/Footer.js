33328de895547ae5a97e6af496b2be3a8c3c1bca
Michael Merlo <michaelmerlo@Michaels-MBP.home>
2019-03-15 16:27:30 -0400

    added in gitignore file

--- components/Footer.js
+++ components/Footer.js
@@ -4,6 +4,7 @@ import Link from 'next/link';
 const Footer = () => (
   <div className="foot">
     <h3>Links to info </h3>
+    <a href="https://my.clevelandclinic.org/health/diseases/15522-gastroparesis/diagnosis-and-tests"> Cleveland Clinic </a>
   </div>
 )
 
